1e2c42971164fb0a5a64bbb3acf1dbb1cd54c946
2200090049 <2200090049@kluniversity.in>
2023-12-21 15:13:18 +0530

    Frist java File for Jerkins

---
+++ AddingTwo.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Hardcoded numbers
+        double num1 = 5.2;
+        double num2 = 3.8;
+
+        // Add the two numbers
+        double sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
